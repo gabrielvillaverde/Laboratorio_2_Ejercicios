72ee959c41b1ab95d2f2b96e9689a319e0e78d47
Gabriel Villaverde <gbr.villaverde@gmail.com>
2022-04-19 22:51:40 -0300

    19/04

---
+++ Clases y métodos estáticos/Ejercicio_I06_Calculadora/Ejercicio_I06_Calculadora/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_I06_Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
